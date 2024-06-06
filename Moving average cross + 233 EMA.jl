c4b49f62b022f056c968fac61686b70a8acfcb42
MartinKasak <martin.kasak11@gmail.com>
2024-06-06 14:17:45 +0300

    Exponential moving average + 233 EMA

--- Moving average cross + 233 EMA.jl
+++ Moving average cross + 233 EMA.jl
@@ -1,15 +1,16 @@
 //@version=4
-strategy(title="Exponential Moving average cross", shorttitle="MA cross", calc_on_every_tick = true,overlay=true, process_orders_on_close=true, default_qty_type=strategy.percent_of_equity, default_qty_value=100)
+strategy(title="Moving average cross+233EMA", shorttitle="MA cross+233EMA", calc_on_every_tick = true,overlay=true, process_orders_on_close=true, default_qty_type=strategy.percent_of_equity, default_qty_value=100)
 
-movingaverage1 =input(9, title="fast moving average")
-mma2002=ema(close, movingaverage1)
-movingaverage2 =input(4, title="slow moving average")
-mma2003=ema(close, movingaverage2)
+movingaverage1 =input(10, title="fast moving average")
+mma2002=sma(close, movingaverage1)
+movingaverage2 =input(6, title="slow moving average")
+mma2003=sma(close, movingaverage2)
 movingaverage3 =input(233, title="slow moving average")
 mma2004=ema(close, movingaverage3)
+
 plot(mma2002, title='fast', color=#06BD1C, linewidth=1)
 plot(mma2003, title='slow', color=#0B60C6, linewidth=1)
-plot(mma2004, title='very slow', color=#ffff00, linewidth=1)
+plot(mma2004, title='very slow', color=#fff700, linewidth=1)
 
 testStartYear = input(2003, "Backtest Start Year")
 testStartMonth = input(1, "Backtest Start Month")
@@ -24,6 +25,5 @@ testPeriod() =>
     time >= testPeriodStart and time <= testPeriodStop ? true : false
     
 if testPeriod()
-    strategy.entry("BuyOS", true, when = crossover(mma2002, mma2003) )
-    strategy.close("BuyOS", when = crossunder(mma2002, mma2003))
-      
+    strategy.entry("BuyOS", true, when = mma2002 > mma2003 and close > mma2004)
+    strategy.close("BuyOS", when = mma2002 < mma2003)      
